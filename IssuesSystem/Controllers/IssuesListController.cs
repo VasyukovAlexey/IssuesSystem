68fe32986a6173b4279f01ec86b1222c61e9c6d3
home <home@SUPER>
2019-08-15 02:25:59 +0500

    Added issue states and datetime resolving.

--- IssuesSystem/Controllers/IssuesListController.cs
+++ IssuesSystem/Controllers/IssuesListController.cs
@@ -1,24 +1,73 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Policy;
 using System.Web;
+using System.Web.Configuration;
 using System.Web.Mvc;
 using IssuesSystem.Models;
 
 namespace IssuesSystem.Controllers
 {
-    public class IssuesListController : Controller
+    public class IssueListToSend
     {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public string State { get; set; }
+        public string DateCreated { get; set; }
+    }
+    
+
+public class IssuesListController : Controller
+    {
+        
+
         // GET: IssuesList
         public ActionResult Index()
         {
-            
+            return View();
+        }
+
+        public ActionResult CollectIssuesList()
+        {
+            Dictionary<int, string> States =
+                new Dictionary<int, string>
+                {
+                    {1, "Открыта"},
+                    {2, "Решена"},
+                    {3, "Возврат"},
+                    {4, "Закрыта"}
+
+
+                };
             using (IssuesSystemDBEntities issuesDB = new IssuesSystemDBEntities())
             {
                 List<Issue> issuesList = issuesDB.Issues.ToList<Issue>();
                 
-            }
-            return View();
+                List< IssueListToSend> issueListToSends  = new List<IssueListToSend>();
+                foreach (var issue in issuesList)
+                {
+                    string s = "";
+                    if (issue.State!=null)
+                    {
+                        States.TryGetValue((int)issue.State, out s);
+                    }
+                    
+                    
+
+                    issueListToSends.Add(
+                        new IssueListToSend()
+                        {
+                            Id = issue.Id,
+                            //State = issue.State,
+                            State = s,
+                            Description = issue.Description,
+                            DateCreated = issue.DateCreated.ToString()
+                        }
+                        );
+                }
+                return Json(new { data = issueListToSends }, JsonRequestBehavior.AllowGet);
+        }
         }
     }
 }
