5edd90366a82f6963cedc0b939de127d4de31b99
home <home@SUPER>
2019-08-15 00:24:12 +0500

    Default route set up. Issues Controller and View added.

--- IssuesSystem/App_Start/RouteConfig.cs
+++ IssuesSystem/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace IssuesSystem
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "IssuesList", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
